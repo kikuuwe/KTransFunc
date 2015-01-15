e33f302ac0fe45cfb4b2f99c489f04e0847ddb38
Ryo Kikuuwe <kikuuwe@ieee.org>
2015-01-15 19:45:45 +0100

    new

--- KBWLPFilter.h
+++ KBWLPFilter.h
@@ -33,12 +33,12 @@ class KBWLPFilter
 {
   public:
   /************************************************************************/  
-  static void Make(KTransFunc* pTF, int Order,double aDeltaT ,double aHerz)
+  static void Make(KTransFunc* pTF, int Order, double aDeltaT, double aHerz)
   {
   // pTF->Construct(((Order==1)?0:Order) , Order);
-    pTF->Construct(Order , Order);
     if(aHerz<=0)
     {
+      pTF->Construct(Order , Order);
       pTF->mB[0] = 1. ;
       for(int p=1;p<=pTF->N;p++) pTF->mB[p]=0.;
       for(int p=1;p<=pTF->M;p++) pTF->mA[p]=0.;
@@ -53,6 +53,7 @@ class KBWLPFilter
     //  Integral Approximation
     //  pTF->mB[0] = 1.- exp(-2. * PI * aHerz * aDeltaT )  ;
     //  pTF->mA[1] =   - exp(-2. * PI * aHerz * aDeltaT ) ;
+      pTF->Construct(Order , Order);
       double tW=2./ aDeltaT * tan( aDeltaT * PI * aHerz );
       double tDenomi = 1./(tW + 2./aDeltaT) ;
       pTF->mB[0] = tDenomi * tW;
@@ -62,6 +63,7 @@ class KBWLPFilter
     if(Order==2)
     {
     // Bilinear Transform
+      pTF->Construct(Order , Order);
       double tDenomi = 1./(1. + sqrt(2.)*tG + tG*tG );
       pTF->mB[0] = tDenomi * tG*tG;
       pTF->mB[1] = 2.* pTF->mB[0];
@@ -71,6 +73,7 @@ class KBWLPFilter
     }
     if(Order==3)
     {
+      pTF->Construct(Order , Order);
       double tDenomi = 1./(1. + tG)/(1. + tG + tG * tG ) ;
       pTF->mB[0]=tDenomi * ( tG*tG*tG )  ;
       pTF->mB[1]= 3.* pTF->mB[0] ;
@@ -82,16 +85,17 @@ class KBWLPFilter
     }
 		if(Order==4)
 		{
-			double tDenomi = 1./(1. + 2.*cos(PI/8)*tG + tG*tG )/(1. + 2.*cos(3.*PI/8)*tG + tG*tG) ;
-			pTF->mB[0]= tDenomi * tG*tG*tG*tG ;
-			pTF->mB[1]= pTF->mB[0]*4. ;
-			pTF->mB[2]= pTF->mB[0]*6. ;
-			pTF->mB[3]= pTF->mB[0]*4. ;
-			pTF->mB[4]= pTF->mB[0] ;
-			pTF->mA[1]= tDenomi* 4.*(-1. + tG*tG)*(1. + (cos(PI/8) +cos(3.*PI/8)) *tG + tG*tG) ;
-			pTF->mA[2]= tDenomi* 2.*( 3. - (2.+sqrt(2.))*tG*tG + 3. *tG*tG*tG*tG) ;
-			pTF->mA[3]= tDenomi* 4.*(-1. + tG*tG)*(1. - (cos(PI/8)+cos(3*PI/8)) * tG  + tG*tG)   ;
-			pTF->mA[4]= tDenomi* (1. - 2.*cos(PI/8)*tG + tG*tG) * (1. - 2.*tG*cos(3.*PI/8) + tG*tG)  ;
+      pTF->Construct(Order , Order);
+	double tDenomi = 1./(1. + 2.*cos(PI/8)*tG + tG*tG )/(1. + 2.*cos(3.*PI/8)*tG + tG*tG) ;
+	pTF->mB[0]= tDenomi * tG*tG*tG*tG ;
+	pTF->mB[1]= pTF->mB[0]*4. ;
+	pTF->mB[2]= pTF->mB[0]*6. ;
+	pTF->mB[3]= pTF->mB[0]*4. ;
+	pTF->mB[4]= pTF->mB[0] ;
+	pTF->mA[1]= tDenomi* 4.*(-1. + tG*tG)*(1. + (cos(PI/8) +cos(3.*PI/8)) *tG + tG*tG) ;
+	pTF->mA[2]= tDenomi* 2.*( 3. - (2.+sqrt(2.))*tG*tG + 3. *tG*tG*tG*tG) ;
+	pTF->mA[3]= tDenomi* 4.*(-1. + tG*tG)*(1. - (cos(PI/8)+cos(3*PI/8)) * tG  + tG*tG)   ;
+	pTF->mA[4]= tDenomi* (1. - 2.*cos(PI/8)*tG + tG*tG) * (1. - 2.*tG*cos(3.*PI/8) + tG*tG)  ;
 	  	}
 		pTF->Reset();
 	}
